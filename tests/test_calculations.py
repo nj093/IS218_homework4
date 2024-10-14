3640732a48348cbded8cecd98905c5324e6f6748
nj093 <91333058+nj093@users.noreply.github.com>
2024-10-14 12:40:09 -0400

    Part 2 files added

---
+++ tests/test_calculations.py
@@ -0,0 +1,13 @@
+from calculator.operations import add, multiply, subtract, divide
+
+def test_addition():
+    assert add(2,2) == 4
+
+def test_subtraction():
+    assert subtract(2,2) == 0
+
+def test_multiplication():
+    assert multiply(2,2) == 4
+
+def test_division():
+    assert divide(2,2) == 1
